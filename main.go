44c31514ba99932404593c24b70d9e26edb831cf
Luiz Antonio Rozendo <32252774+luizantonio26@users.noreply.github.com>
2024-05-17 19:13:44 -0300

    feat: add costum logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/luizantonio26/gopportunities/router"
+import (
+	"github.com/luizantonio26/gopportunities/config"
+	"github.com/luizantonio26/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//initialize config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	//initialize router
 	router.Initialize()
 }
